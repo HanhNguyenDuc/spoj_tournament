0cdbd4ef16b0c810a0f8046ed0a19b9ab916867d
HanhNguyenDuc <nguyenduchanh99tnvn@gmail.com>
2020-01-13 16:01:43 +0700

    abc

--- spoj/settings.py
+++ spoj/settings.py
@@ -16,7 +16,7 @@ import os
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles/')
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
@@ -132,8 +132,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
 
 
 import dj_database_url 
