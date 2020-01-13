edc3af3bc1afc5ea6c5c09f45a6712d0d594aa8e
HanhNguyenDuc <nguyenduchanh99tnvn@gmail.com>
2020-01-13 17:03:39 +0700

    abc

--- spoj/settings.py
+++ spoj/settings.py
@@ -138,4 +138,4 @@ import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-ALLOWED_HOSTS = ['spoj-tour.spoj.com']
+ALLOWED_HOSTS = ['spoj-tour.herokuapp.com']
